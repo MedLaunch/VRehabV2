557cf6a1e3ec68fcc63ff30adb23ebab735a233d
Justin Liu <justin789liu@gmail.com>
2021-02-10 21:11:38 -0500

    added buttons for wine

---
+++ Assets/Scripts/SliderValueToText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;  // text objects value
+    private string initialText;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();  // need this line to interact with text
+        initialText = textSliderValue.text;
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue()
+    {
+        // have to use player to interact with slider rather than in code to 
+        // see the change ... common bug?
+        // e.g. initalText = "Glasses: ", sliderUI.value = 1
+        textSliderValue.text = initialText + " " + sliderUI.value;
+    }
+}
--- Assets/Scripts/WineLevel.cs
+++ Assets/Scripts/WineLevel.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class WineTable : MonoBehaviour
+public class WineLevel : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text percentText;
     public float percent;
-
+    public float totalTimeOut = 0f;
+    public float totalTimeLanded = 0f;  // total time liquid is inside wine
+    public bool drip;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        percent = GameObject.Find("wine glass").GetComponent<IntakeLiquid>().liquid;
+        totalTimeOut = GameObject.Find("wine bottle").GetComponent<PourLiquid>().totalTimeOut;
         SetPercentCount();
     }
     void SetPercentCount()
     {
-        percentText.text = "Percent: " + percent.ToString();
+        percentText.text = "Percent: " + totalTimeLanded.ToString() + " / " + totalTimeOut.ToString();
     }
 
     void OnParticleCollision(GameObject other)
     {
-
+        totalTimeLanded += Time.deltaTime;
     }
 }
