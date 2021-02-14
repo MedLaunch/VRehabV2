b18bfc8bf935e7ae9955512f25397cf2b52038ed
Justin Liu <justin789liu@gmail.com>
2021-02-14 14:19:26 -0500

    wine scripts

---
+++ Assets/Scripts/WineController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WineController : MonoBehaviour
+{
+    public int numGlasses;
+    public GameObject glass;
+    public GameObject clone;
+    public Transform spawn;
+    bool glassSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        glassSpawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (numGlasses > 0)
+        {
+            clone = Instantiate(glass, spawn.position, spawn.rotation);  // Spawns gameObject
+            
+        }
+    }
+}
